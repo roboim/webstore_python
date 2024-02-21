ab820f0a0c2db1f16fc9947a7c4f6479cd2d2aba
Ilya Shevelev <robotedu@yandex.ru>
2024-02-21 16:06:01 +0300

    fix: admin.py adjusted

--- storebackend/admin.py
+++ storebackend/admin.py
@@ -6,20 +6,15 @@ from storebackend.models import Category, Product
 
 @admin.register(Category)
 class CategoryAdmin(ImportExportModelAdmin):
-    list_display = ['id', 'name']
-    search_fields = ['id', 'name']
+    pass
+    # list_display = ['id', 'name']
+    # search_fields = ['id', 'name']
 
 
 @admin.register(Product)
 class ProductAdmin(ImportExportModelAdmin):
-    list_display = ['id', 'name', 'category']
-    search_fields = ['id', 'name', 'category']
-
-# @admin.register(Category)
-# class CategoryAdmin(admin.ModelAdmin):
-#     pass
+    pass
+    # list_display = ['id', 'name', 'category']
+    # search_fields = ['id', 'name', 'category']
 
 
-# @admin.register(Product)
-# class ProductAdmin(admin.ModelAdmin):
-#     pass
