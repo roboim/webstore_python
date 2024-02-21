78c9dd488513c9ff6683c2aa55a5c691be2ec5af
Ilya Shevelev <robotedu@yandex.ru>
2024-02-21 13:31:10 +0300

    feat: url categories/ added. POST method tested

--- client.py
+++ client.py
@@ -1,17 +1,17 @@
 import requests
 
 str_route = 'api/v1/'
-response = requests.get(
-    f'http://127.0.0.1:8000/{str_route}categories/'
-)
-print(response.status_code)
-print(response.text)
-
-# response = requests.post(
-#     'http://127.0.0.1:8000/categories/',
-#     json={'name': 'Категория 1', 'shop': []]}
+# response = requests.get(
+#     f'http://127.0.0.1:8000/{str_route}categories/'
 # )
 # print(response.status_code)
 # print(response.text)
+
+response = requests.post(
+    f'http://127.0.0.1:8000/{str_route}categories/',
+    json={'name': 'Категория 1', 'shop': []}
+)
+print(response.status_code)
+print(response.text)
 
 
