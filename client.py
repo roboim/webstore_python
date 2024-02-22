1a52bcd9d66a4fcae750bba2f913c2c2e6377102
Ilya Shevelev <robotedu@yandex.ru>
2024-02-22 19:13:29 +0300

    refactor: minor adjustments

--- client.py
+++ client.py
@@ -3,7 +3,8 @@ from pprint import pprint
 import requests
 import yaml
 
-str_route = 'api/v1/'
+str_route = 'api/v1/'       # API версия
+filename = 'shop1.yaml'     # Название тестового файла для обновления данных поставщика
 
 # response = requests.get(
 #     f'http://127.0.0.1:8000/{str_route}categories/'
@@ -32,13 +33,12 @@ str_route = 'api/v1/'
 # print(response.text)
 
 # Обновить данные поставщика
-filename = 'shop1.yaml'
 with open(filename, 'r', encoding="utf-8") as f:
-    import_data = yaml.load(f, Loader=yaml.FullLoader)
-    pprint(import_data)
+    export_data = yaml.load(f, Loader=yaml.FullLoader)
+    pprint(export_data)
     response = requests.post(
         f'http://127.0.0.1:8000/{str_route}supplier/data/',
-        data=import_data,
+        data=export_data,
     )
     print(response.status_code)
     print(response.text)
