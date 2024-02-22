7a8ba18b31648b129eec2107de10902e73ac8dd9
Ilya Shevelev <robotedu@yandex.ru>
2024-02-22 20:42:05 +0300

    feat: error_prompt added

--- storebackend/services.py
+++ storebackend/services.py
@@ -8,9 +8,18 @@ def read_yaml_write_to_db(request, *args, **kwargs) -> Response:
     """
     Прочитать yaml файл и выполнить запись в базу данных
     """
-    with open('file.yaml', 'wb') as f:
-        f.write(request.FILES['upload_file'].file.read())
-    with open('file.yaml', encoding='utf-8') as f:
-        import_data = yaml.load(f, Loader=yaml.FullLoader)
-        pprint(import_data)
+    try:
+        import_file = request.FILES['upload_file'].file.read()
+        import_data = yaml.load(import_file, Loader=yaml.FullLoader)
+
+    except Exception as error:
+        return error_prompt(False, "File didn't load", 400)
+
     return Response({'Status': True, 'success': 'Yes'}, status=201)
+
+
+def error_prompt(status_data: bool, error_data: str, code_data: int) -> Response:
+    """
+    Вернуть сообщение об ошибке
+    """
+    return Response({'Status': status_data, 'Error': error_data}, status=code_data)
