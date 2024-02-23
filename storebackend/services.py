2254f541e4670798af949b1e0c793960536d467f
Ilya Shevelev <robotedu@yandex.ru>
2024-02-23 09:48:29 +0300

    feat: clear of product data in case of products updating

--- storebackend/services.py
+++ storebackend/services.py
@@ -3,6 +3,8 @@ from pprint import pprint
 import yaml
 from rest_framework.response import Response
 
+from storebackend.models import Shop, ProductInfo
+
 
 def read_yaml_write_to_db(request, *args, **kwargs) -> Response:
     """
@@ -11,9 +13,17 @@ def read_yaml_write_to_db(request, *args, **kwargs) -> Response:
     try:
         import_file = request.FILES['upload_file'].file.read()
         import_data = yaml.load(import_file, Loader=yaml.FullLoader)
-
     except Exception as error:
         return error_prompt(False, "File didn't load", 400)
+    #  Записать/обновить данные по магазину
+
+    #  !!!!!!!!!!!!!! Добавить владельца магазина после аутентификации
+
+    shop, status_result = Shop.objects.get_or_create(name=import_data['shop'])  #  , user_id=request.user.id)
+    if not status_result:
+        if shop.user_id == request.user.id:
+            ProductInfo.objects.filter(shop_id=shop.id).delete()
+    print(shop, status_result)
 
     return Response({'Status': True, 'success': 'Yes'}, status=201)
 
