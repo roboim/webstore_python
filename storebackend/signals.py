3b7b88e33b07a56223231c15d44609ce5b2ff8c8
Ilya Shevelev <robotedu@yandex.ru>
2024-02-23 17:59:02 +0300

    feat: test mail from signals.py sent

--- storebackend/signals.py
+++ storebackend/signals.py
@@ -1,7 +1,7 @@
 from typing import Type
 
 from django.conf import settings
-from django.core.mail import EmailMultiAlternatives
+from django.core.mail import EmailMultiAlternatives, send_mail
 from django.db.models.signals import post_save
 from django.dispatch import receiver, Signal
 
@@ -11,8 +11,11 @@ new_user_registered = Signal()
 
 
 @receiver(post_save, sender=User)
-def new_user_created_signal(sender, instance, created, **kwargs):
+def new_user_created_signal(sender: Type[User], instance: User, created: bool, **kwargs):
     """
     Отправка письма для подтверждения почтового адреса
     """
     print('SIGNALS')
+    if created and not instance.is_active:
+        message_body = "Тестовое сообщение от Django!"
+        send_mail('Тема', message_body, settings.EMAIL_HOST_USER, ['test@yandex.ru'])
